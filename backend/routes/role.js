1d2fa7011adc26ea64628ba72ac1334702108247
ismailalakhras <ismail.malakhras@gmail.com>
2024-07-21 19:08:33 +0300

    create permission

--- backend/routes/role.js
+++ backend/routes/role.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const { createNewRole } = require("../controllers/role");
+const { createNewPermission } = require("../controllers/role");
 
 
 
 const rolesRouter = express.Router();
 
 rolesRouter.post("/", createNewRole);
+
+rolesRouter.post("/permission", createNewPermission);
+
+
+
+
 
 module.exports = rolesRouter;
